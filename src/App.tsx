4b8cf49ebcaebe474563ef4e9a6b0bc7d44f59ab
Shokhrukh <shoxrux20888@mail.ru>
2024-03-14 23:46:55 +0300

    Getting data using AsyncThunkConfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Header } from './components/Header/Header';
+import Main from './components/Main/Main';
+import { useAppDispatch, useAppSelector } from './hook/hook';
+import { fetchBooks } from './redux/bookSlice';
 
 function App() {
-  return <>Hello, World!</>;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks('java'));
+  }, [dispatch]);
+  return (
+    <>
+      <Header />
+      <Main />
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
