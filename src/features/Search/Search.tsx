189a6d2366125c8ba8cd49c1e809c21c2718e329
Shokhrukh <shoxrux20888@mail.ru>
2024-04-04 22:43:14 +0300

    modified: serch.tsx

--- src/features/Search/Search.tsx
+++ src/features/Search/Search.tsx
@@ -1,24 +1,25 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useAppDispatch, useEmailSelector } from '../../shared/hook/hook';
 import useDebounce from '../../shared/hook/useDebounce';
 import { addItemToHistory } from '../../shared/reducers/slices/userSlice';
 import { SearchList } from '../SearchList/SearchList';
 import st from './Search.module.css';
 
-type Inputs = {
-  search: string;
-};
-
 export const Search = () => {
+  const [searchParams] = useSearchParams();
   const [searchText, setSearchText] = useState('');
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const dispatch = useAppDispatch();
   const debouncedSearchTerm = useDebounce(searchText, 500);
-  const { register } = useForm<Inputs>();
   const navigate = useNavigate();
   const email = useEmailSelector();
+  const bookQuery = searchParams.get('q') ?? '';
+
+  useEffect(() => {
+    setSearchText(bookQuery);
+  }, [bookQuery]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(event.target.value);
@@ -45,16 +46,13 @@ export const Search = () => {
           }),
         );
       }
-
       setIsDropdownOpen(false);
-      setSearchText('');
     }
   };
 
   return (
     <form className={st.search}>
       <input
-        {...register('search')}
         className={st.search_box}
         type="search"
         id="search-box"
