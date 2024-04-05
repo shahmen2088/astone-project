aacc02b9c6db417cb4508ce552180ddffc747c65
Shokhrukh <shoxrux20888@mail.ru>
2024-04-05 06:41:37 +0300

    modified: App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,21 @@
 import { useMemo, useState } from 'react';
 import { Header } from '../components/Header/Header';
 import { ThemeContextMode } from '../shared/contextTheme/ThemeContextMode';
-
 import { AppRouter } from './router/app-router';
 
 function App() {
   const [theme, setTheme] = useState('dark');
-  const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
-  };
 
   const value = useMemo(
-    () => ({ theme, toggleThemeMode: toggleTheme }),
-    [theme, toggleTheme],
+    () => ({
+      theme,
+      toggleThemeMode: () => {
+        setTheme((currentTheme) =>
+          currentTheme === 'light' ? 'dark' : 'light',
+        );
+      },
+    }),
+    [theme],
   );
 
   return (
